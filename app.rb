13688941fc0db85c8c0537efb1c78a7adf912d87
KoshmareG <Koshmareg@yandex.ru>
2022-02-23 21:44:38 +0700

    Add database init and create posts table

--- app.rb
+++ app.rb
@@ -3,8 +3,19 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def init_database
+  @db = SQLite3::Database.new 'Usersposts.db'
+  @db.results_as_hash = true
+  return @db
+end
+
+before do
+  init_database
+end
+
 configure do
-  enable :sessions
+  init_database
+  @db.execute 'create table if not exists "Posts" ("id" integer primary key autoincrement, "postdate" date, "post" text)'
 end
 
 get '/' do
@@ -13,4 +24,9 @@ end
 
 get '/newpost' do
   erb :newpost
+end
+
+post '/newpost' do
+  newuserpost = params[:newuserpost]
+  erb 'Ваш пост отправлен!'
 end
